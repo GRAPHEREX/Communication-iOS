4b76295e673b55538f27e29c887af4066b13f3e0
Evgeniy Zabolotniy <eugenezabolotniy@yandex.ru>
2021-01-28 17:03:50 +0300

    Staging server was configured

--- SignalServiceKit/src/TSConstants.swift
+++ SignalServiceKit/src/TSConstants.swift
@@ -201,52 +201,46 @@ private class TSConstantsProduction: TSConstantsProtocol {
 
 private class TSConstantsStaging: TSConstantsProtocol {
 
-    public let textSecureWebSocketAPI = "wss://textsecure-service-staging.whispersystems.org/v1/websocket/"
-    public let textSecureServerURL = "https://textsecure-service-staging.whispersystems.org/"
-    public let textSecureCDN0ServerURL = "https://cdn-staging.signal.org"
-    public let textSecureCDN2ServerURL = "https://cdn2-staging.signal.org"
-    public let contactDiscoveryURL = "https://api-staging.directory.signal.org"
-    public let keyBackupURL = "https://api-staging.backup.signal.org"
-    public let storageServiceURL = "https://storage-staging.signal.org"
+    public let textSecureWebSocketAPI = "wss://api.grapherextests.com/v1/websocket/"
+    public let textSecureServerURL = "https://api.grapherextests.com/"
+    public let walletServerURL = "https://crypto.grapherextests.com/"
+    public let textSecureCDN0ServerURL = "https://cdn.grapherextests.com"
+    public let textSecureCDN2ServerURL = "https://cdn.grapherextests.com"
+    public let contactDiscoveryURL = "https://directory.grapherex.com"
+    public let keyBackupURL = "https://backup.grapherex.com"
+    public let storageServiceURL = "https://storage.grapherex.com"
     public let sfuURL = "https://sfu.voip.signal.org"
-    public let kUDTrustRoot = "BbqY1DzohE4NUZoVF+L18oUPrK3kILllLEJh2UnPSsEx"
+    public let kUDTrustRoot = "BTdIuRPrgRf3GnJXgjjqddSrLng6VkZTZNDR7qXjmrlz"
 
     public let censorshipReflectorHost = "europe-west1-signal-cdn-reflector.cloudfunctions.net"
 
-    public let serviceCensorshipPrefix = "service-staging"
-    public let cdn0CensorshipPrefix = "cdn-staging"
-    public let cdn2CensorshipPrefix = "cdn2-staging"
-    public let contactDiscoveryCensorshipPrefix = "directory-staging"
-    public let keyBackupCensorshipPrefix = "backup-staging"
-    public let storageServiceCensorshipPrefix = "storage-staging"
+    public let serviceCensorshipPrefix = "service"
+    public let cdn0CensorshipPrefix = "cdn"
+    public let cdn2CensorshipPrefix = "cdn"
+    public let contactDiscoveryCensorshipPrefix = "directory"
+    public let keyBackupCensorshipPrefix = "backup"
+    public let storageServiceCensorshipPrefix = "storage"
 
-    // CDS uses the same EnclaveName and MrEnclave
-    public let contactDiscoveryEnclaveName = "c98e00a4e3ff977a56afefe7362a27e4961e4f19e211febfbb19b897e6b80b15"
+    public let contactDiscoveryEnclaveName = "cd6cfc342937b23b1bdd3bbf9721aa5615ac9ff50a75c5527d441cd3276826c9"
     public var contactDiscoveryMrEnclave: String {
         return contactDiscoveryEnclaveName
     }
 
     public let keyBackupEnclave = KeyBackupEnclave(
-        name: "dcd2f0b7b581068569f19e9ccb6a7ab1a96912d09dde12ed1464e832c63fa948",
-        mrenclave: "9db0568656c53ad65bb1c4e1b54ee09198828699419ec0f63cf326e79827ab23",
-        serviceId: "89e160bfc95aa13e71caeea0cdbf3492b41c7ce3fc7c093112af7825f396682b"
+        name: "fe7c1bfae98f9b073d220366ea31163ee82f6d04bead774f71ca8e5c40847bfe",
+        mrenclave: "a3baab19ef6ce6f34ab9ebb25ba722725ae44a8872dc0ff08ad6d83a9489de87",
+        serviceId: "fe7c1bfae98f9b073d220366ea31163ee82f6d04bead774f71ca8e5c40847bfe"
     )
 
     // An array of previously used enclaves that we should try and restore
     // key material from during registration. These must be ordered from
     // newest to oldest, so we check the latest enclaves for backups before
     // checking earlier enclaves.
-    public let keyBackupPreviousEnclaves = [
-        KeyBackupEnclave(
-            name: "823a3b2c037ff0cbe305cc48928cfcc97c9ed4a8ca6d49af6f7d6981fb60a4e9",
-            mrenclave: "a3baab19ef6ce6f34ab9ebb25ba722725ae44a8872dc0ff08ad6d83a9489de87",
-            serviceId: "038c40bbbacdc873caa81ac793bb75afde6dfe436a99ab1f15e3f0cbb7434ced"
-        )
-    ]
+    public let keyBackupPreviousEnclaves = [KeyBackupEnclave]()
 
     public let applicationGroup = "group.app.grapherex"
 
     // We need to discard all profile key credentials if these values ever change.
     // See: GroupsV2Impl.verifyServerPublicParams(...)
-    public let serverPublicParamsBase64 = "ABSY21VckQcbSXVNCGRYJcfWHiAMZmpTtTELcDmxgdFbtp/bWsSxZdMKzfCp8rvIs8ocCU3B37fT3r4Mi5qAemeGeR2X+/YmOGR5ofui7tD5mDQfstAI9i+4WpMtIe8KC3wU5w3Inq3uNWVmoGtpKndsNfwJrCg0Hd9zmObhypUnSkfYn2ooMOOnBpfdanRtrvetZUayDMSC5iSRcXKpdls=="
+    public let serverPublicParamsBase64 = "DDZM414H2QbA3brAa6NCMaZIN1ZRY+B46PWDvw4LmwrY6CEQArF4OF/yHdBL7HW/JPgjjauzJau+cpikvqH3dDZQ7KFKgx/MGsbw49ATUj6fhBXko9iyPwVwC3+kjNY6PGZuSoYpD4SJJIgzTJ8Gnuk23tSbX1aQWAWNlc8WiyWIHm/A+22w/D1zQmGuFCEGImU4blMK+HhNfC7jM5leBQ=="
 }
