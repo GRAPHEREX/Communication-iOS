97daf740503193c25b1627819c9a984f08412bd2
Roman Filippov <rf@stacle.com>
2021-05-31 17:41:59 +0300

    implementing money sending from chat/contacts

--- Signal/GrapherexFoundation/Extensions/UIViewController + Send.swift
+++ Signal/GrapherexFoundation/Extensions/UIViewController + Send.swift
@@ -3,70 +3,16 @@
 // 
 
 import Foundation
+import CryptoWallet
 
 @objc
 extension UIViewController {
     public func showSendFromChat(recipient: SignalRecipient, isOpenedFromContacts: Bool) {
-//        let sendController = SendCurrencyFromChatController()
-//        sendController.recipient = recipient
-//        sendController.isOpenedFromContacts = isOpenedFromContacts
-//
-//        let walletModel = WalletModel.shared
-//        ModalActivityIndicatorViewController.present(
-//            fromViewController: self,
-//            canCancel: true,
-//            backgroundBlock: { [weak self] modal in
-//                guard let self = self else { return }
-//                walletModel.initWallets { result in
-//                    switch result {
-//                    case .success(_):
-//                        walletModel.getRecipientWallets(accountId: recipient.address.phoneNumber!) { result in
-//                            switch result {
-//                            case .success(let recipientWallets):
-//                                sendController.recipeintWallets = recipientWallets
-//
-//                                let myCurrencies: [Currency] = walletModel.wallets.map { return $0.currency }
-//                                let recipientCurrencies: [Currency] = recipientWallets.map { return $0.currency }
-//                                let currencies: [Currency] = myCurrencies.filter { recipientCurrencies.contains($0) }
-//
-//                                let allowedCurrencies = currencies.removeDuplicates()
-//                                sendController.allowedCurrencies = allowedCurrencies
-//
-//                                modal.dismiss {
-//                                    if walletModel.wallets.isEmpty {
-//                                        let controller = NoWalletController()
-//                                        controller.onDoneButtonClicked = { [weak self] in
-//                                            controller.dismiss(animated: true, completion: { [weak self] in
-//                                                self?.presentActionSheet(NewWalletController())
-//                                            })
-//                                        }
-//                                        OWSActionSheets.showActionSheet(controller)
-//                                    } else if allowedCurrencies.isEmpty {
-//                                        OWSActionSheets.showActionSheet(
-//                                            title: "No currencies are currently enabled",
-//                                            message: "You can't send money to this user, as he does not have available wallets",
-//                                            image: #imageLiteral(resourceName: "Wallet")
-//                                        )
-//                                    } else {
-//                                        sendController.hidesBottomBarWhenPushed = true
-//                                        self.navigationController?.pushViewController(sendController, animated: true)
-//                                    }
-//                                }
-//                            case .failure(let error):
-//                                print(error)
-//                                modal.dismiss {
-//                                    self.handleError(error: error)
-//                                }
-//                            }
-//                        }
-//                    case .failure(let error):
-//                        print(error)
-//                        modal.dismiss {
-//                            self.handleError(error: error)
-//                        }
-//                    }
-//                }
-//        })
+        guard let userId = recipient.recipientPhoneNumber else { return }
+        let sendController = AppEnvironment.shared.wallet.createSendMoneyController(forUserId: userId) { [weak self] in
+            self?.navigationController?.dismiss(animated: true, completion: nil)
+        }
+        navigationController?.present(sendController, animated: true)
     }
     
     public func showSendFromChat(recipientAddress: SignalServiceAddress) {
