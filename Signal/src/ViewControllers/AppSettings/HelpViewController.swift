080d7007945feaf6cb600b32b7428f455b52f411
Roman Filippov <rf@stacle.com>
2021-07-07 17:10:28 +0300

    temporary disabled 'contact us' popup from help menu screen

--- Signal/src/ViewControllers/AppSettings/HelpViewController.swift
+++ Signal/src/ViewControllers/AppSettings/HelpViewController.swift
@@ -18,12 +18,12 @@ final class HelpViewController: OWSTableViewController2 {
                                           comment: "Title for support page in app settings.")
         let supportCenterLabel = NSLocalizedString("HELP_SUPPORT_CENTER",
                                                    comment: "Help item that takes the user to the Signal support website")
-        let contactLabel = NSLocalizedString("HELP_CONTACT_US",
-                                             comment: "Help item allowing the user to file a support request")
-        let localizedSheetTitle = NSLocalizedString("EMAIL_SIGNAL_TITLE",
-                                                    comment: "Title for the fallback support sheet if user cannot send email")
-        let localizedSheetMessage = NSLocalizedString("EMAIL_SIGNAL_MESSAGE",
-                                                      comment: "Description for the fallback support sheet if user cannot send email")
+//        let contactLabel = NSLocalizedString("HELP_CONTACT_US",
+//                                             comment: "Help item allowing the user to file a support request")
+//        let localizedSheetTitle = NSLocalizedString("EMAIL_SIGNAL_TITLE",
+//                                                    comment: "Title for the fallback support sheet if user cannot send email")
+//        let localizedSheetMessage = NSLocalizedString("EMAIL_SIGNAL_MESSAGE",
+//                                                      comment: "Description for the fallback support sheet if user cannot send email")
 
         let contents = OWSTableContents()
         contents.title = helpTitle
@@ -36,22 +36,23 @@ final class HelpViewController: OWSTableViewController2 {
                 self?.present(vc, animated: true, completion: nil)
             }
         ))
-        helpSection.add(.disclosureItem(
-            withText: contactLabel,
-            actionBlock: {
-                guard ComposeSupportEmailOperation.canSendEmails else {
-                    let fallbackSheet = ActionSheetController(title: localizedSheetTitle,
-                                                              message: localizedSheetMessage)
-                    let buttonTitle = NSLocalizedString("BUTTON_OKAY", comment: "Label for the 'okay' button.")
-                    fallbackSheet.addAction(ActionSheetAction(title: buttonTitle, style: .default))
-                    self.presentActionSheet(fallbackSheet)
-                    return
-                }
-                let supportVC = ContactSupportViewController()
-                let navVC = OWSNavigationController(rootViewController: supportVC)
-                self.presentFormSheet(navVC, animated: true)
-            }
-        ))
+        // Temporary disabled
+//        helpSection.add(.disclosureItem(
+//            withText: contactLabel,
+//            actionBlock: {
+//                guard ComposeSupportEmailOperation.canSendEmails else {
+//                    let fallbackSheet = ActionSheetController(title: localizedSheetTitle,
+//                                                              message: localizedSheetMessage)
+//                    let buttonTitle = NSLocalizedString("BUTTON_OKAY", comment: "Label for the 'okay' button.")
+//                    fallbackSheet.addAction(ActionSheetAction(title: buttonTitle, style: .default))
+//                    self.presentActionSheet(fallbackSheet)
+//                    return
+//                }
+//                let supportVC = ContactSupportViewController()
+//                let navVC = OWSNavigationController(rootViewController: supportVC)
+//                self.presentFormSheet(navVC, animated: true)
+//            }
+//        ))
         contents.addSection(helpSection)
 
         let loggingSection = OWSTableSection()
